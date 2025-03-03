99a6dfa9a40d4e8abd24685ee44a8643a6ae45a2
Utkarsh <utkarshrenukdas2003@gmail.com>
2025-03-03 12:14:21 +0530

    Fix case issue for FAQ.css

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import WhyFree from './components/WhyFree';
 import HowItWorks from './components/HowItWorks';
 import Legacy from './components/Legacy';
 import Stats from './components/Stats';
-import FAQ from './components/Faq';
+import FAQ from './components/FAQ';
 import CTA from './components/CTA';
 import Footer2 from './components/Footer2';
 import ProgressBar from './components/ProgressBar';
